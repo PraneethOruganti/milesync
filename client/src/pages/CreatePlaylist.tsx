27cdd251b84e59e578c2bf20087ed8e167da09cc
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2026-04-25 18:12:11 -0400

    created initial playlistform to enter in mile data

---
+++ client/src/pages/CreatePlaylist.tsx
@@ -0,0 +1,10 @@
+import { PlaylistForm } from "../components/PlaylistForm"
+
+export const CreatePlaylist = () => {
+    return (
+        <div>
+            <h1>CreatePlaylist</h1>
+            <PlaylistForm onSubmit={() => {}} />
+        </div>
+    )
+}
